47e00b0f956c4ca07e2768bd2b17c11d8f926923
ReddiMadhu <83718786+ReddiMadhu@users.noreply.github.com>
2025-06-12 11:11:24 +0530

    Update Olympics.sql

--- Olympics.sql
+++ Olympics.sql
@@ -22,3 +22,50 @@ GROUP BY
 ORDER BY 
     Count_Gold_Medals DESC
 LIMIT 1;
+
+
+
+#--2 for each team print total silver medals and year in which they won maximum silver medal..output 3 columns
+-- team,total_silver_medals, year_of_max_silver
+WITH team_year_silver AS (
+    SELECT 
+        athletes.team AS team,
+        ae.year AS year,
+        COUNT(*) AS silver_count
+    FROM 
+        athlete_events ae
+    JOIN 
+        athletes ON ae.athlete_id = athletes.id
+    WHERE 
+        ae.medal = 'Silver'
+    GROUP BY 
+        athletes.team, ae.year
+),
+ranked_silver_years AS (
+    SELECT *,
+           RANK() OVER (PARTITION BY team ORDER BY silver_count DESC) AS rnk
+    FROM team_year_silver
+),
+team_total_silver AS (
+    SELECT 
+        athletes.team AS team, 
+        COUNT(*) AS total_silver_medals
+    FROM 
+        athlete_events ae
+    JOIN 
+        athletes ON ae.athlete_id = athletes.id
+    WHERE 
+        ae.medal = 'Silver'
+    GROUP BY 
+        athletes.team
+)
+SELECT 
+    tts.team,
+    tts.total_silver_medals,
+    rsy.year AS year_of_max_silver
+FROM 
+    team_total_silver tts
+JOIN 
+    ranked_silver_years rsy ON tts.team = rsy.team
+WHERE 
+    rsy.rnk = 1;
